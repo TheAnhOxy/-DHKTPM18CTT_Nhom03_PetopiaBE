19ec2c95807f1e6dad097241d751f07b512cf2b7
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2025-11-26 04:05:27 +0700

    style: format code

--- src/main/java/com/pet/controller/web/PaymentController.java
+++ src/main/java/com/pet/controller/web/PaymentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/payments")
 public class PaymentController {
 
-    @Autowired private OrderServiceImpl orderService; // Nơi chứa hàm processSePayPayment
+    @Autowired private OrderServiceImpl orderService; 
 
     // API này sẽ được SePay gọi tự động (Bạn cần config trên SePay)
     @PostMapping("/sepay-webhook")
