98bbccb38a7eda3b15830abc1cb6e69307e59a50
Tran Pham Bao Khang <Baokhang8a1@gmail.com>
2025-11-27 19:35:03 +0700

    sửa lỗi comment

--- src/main/java/com/pet/controller/admin/ArticleCommentController.java
+++ src/main/java/com/pet/controller/admin/ArticleCommentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/article-comments")
+@RequestMapping("/api/article-comments")
 public class ArticleCommentController {
 
     @Autowired
--- src/main/java/com/pet/controller/admin/ArticleController.java
+++ src/main/java/com/pet/controller/admin/ArticleController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/articles")
+@RequestMapping("/api/articles")
 public class ArticleController {
 
     @Autowired
